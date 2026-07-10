6a0c36560671dded51c3935c419a7dbf7cc8c3e5
Jiahan Chen <chenjiahan.jait@bytedance.com>
2026-07-10 16:55:05 +0800

    chore: migrate tests to Rstest (#72)

--- test/basic/index.test.ts
+++ test/basic/index.test.ts
@@ -1,7 +1,7 @@
 import { readFileSync } from 'node:fs';
 import { dirname, join } from 'node:path';
 import { fileURLToPath } from 'node:url';
-import { expect, test } from '@playwright/test';
+import { expect, test } from '@rstest/playwright';
 import { createRsbuild } from '@rsbuild/core';
 import { pluginStyledComponents } from '../../dist';
 
